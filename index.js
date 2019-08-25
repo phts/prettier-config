cec12324fe51cee48e429cb5645395f637c37fa5
Phil Tsaryk <phil@tsaryk.com>
2019-08-25 10:25:02 +0200

    Remove default props

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
   bracketSpacing: false,
-  htmlWhitespaceSensitivity: 'css',
-  jsxBracketSameLine: false,
   printWidth: 100,
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
-  requirePragma: false,
   semi: false,
   singleQuote: true,
-  tabWidth: 2,
   trailingComma: 'all',
 }
